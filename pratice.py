240a82c9c1c63765dd4e07ace5e478ddce83d369
K AKASH <kakashsunny2007@gmail.com>
2026-09-01 23:58:24 +0530

    Add files via upload

---
+++ pratice.py
@@ -0,0 +1,13 @@
+a='hello'
+print(a[::-1])
+
+
+n=int(input("enter the num:"))
+if(all(n%i!=0 for i in range(2,int(n**0.5)+1))):
+    print("prime")
+else:
+    print("Not prime")
+
+p=10/
++20+30
+print(p)
